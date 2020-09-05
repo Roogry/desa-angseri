5dd63c0715c109f7b8a9e4f1feecdebabd1edf52
Roogry <jodiemantra@gmail.com>
2020-09-05 21:03:40 +0800

    get post from api

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,22 +21,41 @@ function scrollTop() {
    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
-function loadPost() {
-   let url = 'https://svbaksosti.herokuapp.com/posts';
+var postCount = 99;
+var action = 'inactive';
+const defaultThumbnail = 'assets/img/background/blog.jpg';
 
-   fetch(url).then(response => response.json())
-      .then(function (data) {
-         var template = data.map(post => {
 
-            let fullContent = marked(post.content);
-            let divHTML = $('<div>').html(fullContent);
-            let pFirst = divHTML.find('p:first')[0].innerText;
+function getPosts(limit, start) {
+   let url = `https://svbaksosti.herokuapp.com/posts?_limit=${limit}&_start=${start}`;
 
-            return `<div class="lqd-column col-md-4 col-sm-6 post-list">
+   if (action == 'inactive') {
+      action = 'active';
+
+      fetch(url).then(response => response.json())
+         .then(function (data) {
+
+            postCount = data.countTotal;
+
+            let template = data.posts.map(post => {
+
+               let fullContent = marked(post.content);
+               let divHTML = $('<div>').html(fullContent);
+               let pFirst = divHTML.find('p:first')[0].innerText;
+
+               $('#load-data').html('<p class="text-center">Loading...</p>');
+
+               if (post.thumbnail) {
+                  imgUrl = data.thumbnail.formats.small.url;
+               } else {
+                  imgUrl = defaultThumbnail;
+               }
+
+               return `<div class="lqd-column col-md-4 col-sm-6 post-list">
                         <article class="liquid-lp mb-60">
                            <figure class="liquid-lp-media">
                               <a href="post-detail.html?s=${post.slug}">
-                                 <img src="${post.thumbnail.url}" alt="Lates Post">
+                                 <img src="${imgUrl}" alt="Lates Post">
                               </a>
                            </figure>
                            <header class="liquid-lp-header">
@@ -49,11 +68,40 @@ function loadPost() {
                         </article>
                      </div>
                         `;
-         });
+            });
 
-         $("#list-post").html(template);
+            $("#list-post").append(template);
+            $('#load-data').html('');
+         }).catch(function (e) {
+            console.log(e);
+            $("#berita").hide();
+         });
+      action = 'inactive';
+   } else {
+      $('#load-data').html('<p class="text-center">Done</p>');
+   }
+}
+
+function getPost(slug) {
+   let url = `https://svbaksosti.herokuapp.com/posts/${slug}`;
+
+   fetch(url).then(response => response.json())
+      .then(function (post) {
+         if (post.thumbnail) {
+            imgUrl = post.thumbnail.formats.small.url;
+            imgUrlBig = post.thumbnail.url;
+         } else {
+            imgUrl = defaultThumbnail;
+            imgUrlBig = defaultThumbnail;
+         }
+
+         let fullContent = marked(post.content);
+         $('.blog-single-content').html(fullContent);
+         $('h1.entry-title').text(post.title);
+         $('time.published').text(formatDate(post.created_at));
+         $('div.thumbnail').css("background-image", `url(${imgUrl})`);
+         $('a.thumbnail').attr("href", imgUrlBig);
       }).catch(function (e) {
-         alert(e + " \nGagal Memuat Berita");
-         $("#berita").hide();
+         console.log(e);
       });
 }
